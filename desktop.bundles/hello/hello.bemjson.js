c7c014e00c93555cc226b8a311da02574dfcb94a
Кирилл Э. Федотов <kirillback@yamoney.ru>
2019-05-18 11:12:57 +0300

    create grid

--- desktop.bundles/hello/hello.bemjson.js
+++ desktop.bundles/hello/hello.bemjson.js
@@ -16,10 +16,101 @@ module.exports = {
 		mods: { color: 'whitepaper-default', space: 'default', size: 'default', gap: 'small', menu: 'default', breakpoint: 'default', font: 'ibm' }
 	},
 	content: [
+
 		{
-			block: 'text',
-			mods: { size: 'xxl', view: 'primary' },
-			content: 'Hi! Write your first block here.'
+			block: 'tpl-layout',
+			attrs: { style: 'margin-top: 40px;'},
+			content: [
+				{
+					elem: 'content',
+					content: {
+						elem: 'container',
+						elemMods: { size: 'm', align: 'center' },
+						content: [
+							{
+								elem: 'container',
+								elemMods: { size: 'm', align: 'center' },
+								content: [
+									{
+										block: 'tpl-grid',
+										mods: { 'xs-ratio': '1', 'm-ratio': '1-1', 'col-gap': 'full', 'row-gap': 'full' },
+										content: [
+											{
+												block: 'rectangle',
+												attrs: { style: 'background: #555; width: 100%; height: 398px;' }
+											},
+											{
+												block: 'rectangle',
+												attrs: { style: 'background: #555; width: 100%; height: 398px;' }
+											}
+										]
+									},
+									{
+										block: 'tpl-grid',
+										attrs: { style: 'margin-top: 60px;'},
+										mods: { 'xs-ratio': '1', 's-ratio': '1-1', 'm-ratio': '1-1-1', 'l-ratio': '1-1-1-1', 'col-gap': 'full', 'row-gap': 'full' },
+										content: [
+											{
+												block: 'rectangle',
+												attrs: { style: 'background: #555; width: 100%; height: 398px;' }
+											},
+											{
+												block: 'rectangle',
+												attrs: { style: 'background: #555; width: 100%; height: 398px;' }
+											},
+											{
+												block: 'rectangle',
+												attrs: { style: 'background: #555; width: 100%; height: 398px;' }
+											},
+											{
+												block: 'rectangle',
+												attrs: { style: 'background: #555; width: 100%; height: 398px;' }
+											}
+										]
+									},
+									{
+										block: 'tpl-grid',
+										attrs: { style: 'margin-top: 60px; margin-bottom: 60px;'},
+										mods: { 'xs-columns': '3', 's-columns': '6', 'm-columns': '12', 'col-gap': 'full', 'row-gap': 'full' },
+										content: [
+											{
+												elem: 'fraction',
+												elemMods: { 'xs-col': '3', 's-col': '6', 'm-col': '7', 'l-col': '7', 's-row': '2' },
+												content: [
+													{
+														block: 'rectangle',
+														attrs: { style: 'background: #666; width: 100%; height: 600px;' }
+													}
+												]
+											},
+											{
+												elem: 'fraction',
+												elemMods: { 'xs-col': '3', 's-col': '6', 'm-col': '5', 'l-col': '5' },
+												content: [
+													{
+														block: 'rectangle',
+														attrs: { style: 'background: #555; width: 100%; height: 150px; min-height: 100%;' }
+													}
+												]
+											},
+											{
+												elem: 'fraction',
+												elemMods: { 'xs-col': '3', 's-col': '6', 'm-col': '5', 'l-col': '5' },
+												content: [
+													{
+														block: 'rectangle',
+														attrs: { style: 'background: #555; width: 100%; height: 150px; min-height: 100%;' }
+													}
+												]
+											}
+										]
+									}
+								]
+							}
+						]
+					}
+				}
+			]
 		}
 	]
 };
